358fbf082b999f6c7a48a449f1cafb6264bc4331
SALAH-NAME <phone4ouahidi@gmail.com>
2023-08-12 10:06:23 +0000

    3. BaseModel fix 1

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 """
-This module defines the BaseModel class, which serves as the base class for all
-other classes in the project. It provides common attributes and methods that
-can be inherited by other classes, such as a unique id, created_at and
-updated_at timestamps, and methods for saving and converting the instance to a
-dictionary representation.
-
-The BaseModel class is part of a larger project that involves creating
-a command interpreter for managing objects in a storage system. This class
-provides the foundation for building more complex classes that can be stored
-and managed by the command interpreter.
+This module defines the BaseModel class
 """
 import uuid
 from datetime import datetime
@@ -38,7 +29,7 @@ class BaseModel:
     def to_dict(self):
         """Return a dictionary representation of the instance."""
         # Create a copy of the instance's __dict__ attribute
-        d = dict(self.__dict__)
+        d = self.__dict__.copy()
         # Add the class name to the dictionary under the key '__class__'
         d["__class__"] = self.__class__.__name__
         # Convert the created_at and updated_at attributes to ISO format strs
