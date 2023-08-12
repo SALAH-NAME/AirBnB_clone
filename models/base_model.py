cde4c1f7082409c94a3fe93ebcbc988086e61b3e
SALAH-NAME <phone4ouahidi@gmail.com>
2023-08-12 10:21:46 +0000

    3. BaseModel fix 3

--- models/base_model.py
+++ models/base_model.py
@@ -10,29 +10,51 @@ class BaseModel:
     """Defines all common attributes/methods for other classes."""
 
     def __init__(self):
-        """Initialize a new instance of the BaseModel class."""
+        """
+        Initialize a new instance of the BaseModel class.
+        Args:
+            self (BaseModel): the current instance
+            args (any): not used here
+            kwargs (dict): dictionary of key/value pairs attributes
+        """
         # Assign a unique id to the instance using uuid.uuid4()
         self.id = str(uuid.uuid4())
         # Assign the current datetime to the created_at and updated_at atributs
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-    def __str__(self):
-        """Return a string representation of the instance."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
-                                     self.__dict__)
-
     def save(self):
-        """Update the updated_at attribute with the current datetime."""
+        """
+        Update the updated_at attribute with the current datetime.
+        Args:
+            self (BaseModel): the current instance
+        """
         self.updated_at = datetime.today()
 
     def to_dict(self):
-        """Return a dictionary representation of the instance."""
+        """
+        Return a dictionary representation of the instance.
+        Args:
+            self (BaseModel): the current instance
+        Returns:
+            dict: a dictionary representation of the instance
+        """
         # Create a copy of the instance's __dict__ attribute
         d = self.__dict__.copy()
-        # Add the class name to the dictionary under the key '__class__'
-        d["__class__"] = self.__class__.__name__
         # Convert the created_at and updated_at attributes to ISO format strs
         d["created_at"] = self.created_at.isoformat()
         d["updated_at"] = self.updated_at.isoformat()
+        # Add the class name to the dictionary under the key '__class__'
+        d["__class__"] = self.__class__.__name__
         return d
+
+    def __str__(self):
+        """
+        Return a string representation of the instance.
+        Args:
+            self (BaseModel): the current instance
+        Returns:
+            str: a string representation of the instance
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
