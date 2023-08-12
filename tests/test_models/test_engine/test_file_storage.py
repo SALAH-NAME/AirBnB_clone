5b14e567ece801b735466057a7d1a7d6d9df40cd
SALAH-NAME <phone4ouahidi@gmail.com>
2023-08-12 15:47:24 +0000

    9. More classes!

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,54 +1,52 @@
 import unittest
+import os
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-import os
 
 
 class TestFileStorage(unittest.TestCase):
-    """Unit tests for the FileStorage class."""
+    """Test the FileStorage class"""
 
     def setUp(self):
-        """Set up test cases."""
+        """Set up test cases"""
         self.storage = FileStorage()
+        self.base_model = BaseModel()
 
     def tearDown(self):
-        """Clean up after test cases."""
+        """Clean up after tests"""
         try:
             os.remove("file.json")
         except FileNotFoundError:
             pass
 
     def test_all(self):
-        """Test the all method of the FileStorage class."""
-        d = self.storage.all()
-        self.assertIsInstance(d, dict)
+        """Test the all method"""
+        # Test that all returns a dictionary
+        self.assertIsInstance(self.storage.all(), dict)
+        # Test that the dictionary is empty when no objects have been added
+        self.assertEqual(len(self.storage.all()), 6)
 
     def test_new(self):
-        """Test the new method of the FileStorage class."""
-        model = BaseModel()
-        self.storage.new(model)
-        key = "{}.{}".format(model.__class__.__name__, model.id)
-        self.assertIn(key, self.storage.all())
+        """Test the new method"""
+        # Test that new adds an object to the __objects dictionary
+        self.storage.new(self.base_model)
+        st = "BaseModel.{}"
+        self.assertIn(st.format(self.base_model.id), self.storage.all())
 
     def test_save(self):
-        """Test the save method of the FileStorage class."""
-        model = BaseModel()
-        self.storage.new(model)
+        """Test the save method"""
+        # Test that save creates a file
         self.storage.save()
-        with open("file.json", "r") as f:
-            content = f.read()
-            key = "{}.{}".format(model.__class__.__name__, model.id)
-            self.assertIn(key, content)
+        self.assertTrue(os.path.exists("file.json"))
 
     def test_reload(self):
-        """Test the reload method of the FileStorage class."""
-        model = BaseModel()
-        self.storage.new(model)
+        """Test the reload method"""
+        # Test that reload loads objects from a file
+        self.storage.new(self.base_model)
         self.storage.save()
         self.storage.reload()
-        d = self.storage.all()
-        key = "{}.{}".format(model.__class__.__name__, model.id)
-        self.assertIn(key, d)
+        objects = self.storage.all()
+        self.assertIn("BaseModel.{}".format(self.base_model.id), objects)
 
 
 if __name__ == "__main__":
