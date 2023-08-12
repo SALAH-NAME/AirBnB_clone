44de73fe8f1ba524b3d62a838f4ca83bbb58de5c
SALAH-NAME <phone4ouahidi@gmail.com>
2023-08-12 09:46:35 +0000

    3. BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,10 @@ can be inherited by other classes, such as a unique id, created_at and
 updated_at timestamps, and methods for saving and converting the instance to a
 dictionary representation.
 
-The BaseModel class is part of a larger project that involves creating a command
-interpreter for managing objects in a storage system. This class provides the
-foundation for building more complex classes that can be stored and managed by
-the command interpreter.
+The BaseModel class is part of a larger project that involves creating
+a command interpreter for managing objects in a storage system. This class
+provides the foundation for building more complex classes that can be stored
+and managed by the command interpreter.
 """
 import uuid
 from datetime import datetime
@@ -22,7 +22,7 @@ class BaseModel:
         """Initialize a new instance of the BaseModel class."""
         # Assign a unique id to the instance using uuid.uuid4()
         self.id = str(uuid.uuid4())
-        # Assign the current datetime to the created_at and updated_at attributes
+        # Assign the current datetime to the created_at and updated_at atributs
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
@@ -41,7 +41,7 @@ class BaseModel:
         d = dict(self.__dict__)
         # Add the class name to the dictionary under the key '__class__'
         d["__class__"] = self.__class__.__name__
-        # Convert the created_at and updated_at attributes to ISO format strings
+        # Convert the created_at and updated_at attributes to ISO format strs
         d["created_at"] = self.created_at.isoformat()
         d["updated_at"] = self.updated_at.isoformat()
         return d
